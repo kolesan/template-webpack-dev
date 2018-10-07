525fcbd801718b1bb7807d0424071c3f491d88d5
kolesan <kolesan90@gmail.com>
2018-10-07 08:30:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# mindful
+Simple timer with configurable gongs, to be used during meditation practice
+
+### Running
+Use `npm run dev` to start a webpack development server
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development'
+};
